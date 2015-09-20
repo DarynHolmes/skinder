abefe7a41c3cfd4f812ac2b3b2297a56bb687936
DarynHolmes <DarynHolmes@gmail.com>
2015-09-20 23:11:59 +0100

    Added admin user

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,12 @@
 class Message < ActiveRecord::Base
+  before_save :downcase_email
+
   belongs_to :event
   validates :event, presence: true
   validates :body, :presence => true
  
-
+  private
+  def downcase_email
+      self.email = email.downcase if email.present?
+  end
 end
